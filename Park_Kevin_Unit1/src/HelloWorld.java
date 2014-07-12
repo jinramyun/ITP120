c9c67ce96e9935f902e1c2fe4c8d61ab24f3a6fb
jinramyun <jinramyun@gmail.com>
2014-07-12 00:22:40 -0400

    Add Unit1 Project

---
+++ Park_Kevin_Unit1/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @author: Kevin Park
+ * Unit 1
+ * Prints out statement saying "Hello World" when run
+ **/
+
+
+public class HelloWorld {
+
+	//main method
+	public static void main (String [] args) {
+		//print out statement "Hello World"
+		System.out.println("Hello World");
+	}
+}
